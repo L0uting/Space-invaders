dcb1289837394c5b4e4787f278a6642448ce3ee2
L0uting <alfredparaire@gmail.com>
2026-02-13 00:09:35 +0100

    az

--- game.py
+++ game.py
@@ -6,37 +6,40 @@ pygame.mixer.init()
 clock = pygame.time.Clock()
 tire = pygame.mixer.Sound("Assets/tire.mp3")
 tire.set_volume(0.1)
+boom = pygame.mixer.Sound("Assets/boom.mp3")
+boom.set_volume(3)
 pygame.mixer.music.load("Assets/son.mp3")
-pygame.mixer.music.set_volume(0.4)
+display=(1000,1072)
+pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1) 
 TAILLE = 16
-font = pygame.font.SysFont("Arial", 40)
+font = pygame.font.Font("Assets/ttf/PixelCode.ttf", 40)
 hitbox="oui"
-k=0
+k=49
+score=0
 tick = 0
 murs=[]
-enemis_xy=[ 
-
-(1, 10),
-(1, 20),
-(1, 30),
-(11, 10),
-(11, 20),
-(11, 30),
-(21, 10),
-(21, 20),
-(21, 30),
-(31, 10),
-(31, 20),
-(31, 30),
-(41, 10),
-(41, 20),
-(41, 30),
-
+enemis_xy = [ 
+    (1, 10),
+    (1, 20),
+    (1, 30),
+    (11, 10),
+    (11, 20),
+    (11, 30),
+    (21, 10),
+    (21, 20),
+    (21, 30),
+    (31, 10),
+    (31, 20),
+    (31, 30),
+    (41, 10),
+    (41, 20),
+    (41, 30),
 ]
+
 enemis=[]
 for x,y in enemis_xy:
-   rect = pygame.Rect(x * TAILLE, y * TAILLE, 115, 90)
+   rect = pygame.Rect(x * TAILLE, (y * TAILLE)-100, 115, 90)
    enemis.append(rect)
 
    
@@ -44,7 +47,7 @@ for x,y in enemis_xy:
 direction = 1
 projectiles = []
 
-screen = pygame.display.set_mode((1920, 1072))
+screen = pygame.display.set_mode((display))
 monster_img = pygame.image.load("Assets/monstre2.png").convert_alpha()
 monster2_img = pygame.image.load("Assets/monstre1.png").convert_alpha()
 gal = pygame.image.load("Assets/galaxy.png").convert_alpha()
@@ -53,7 +56,8 @@ vaissea=pygame.image.load("Assets/vaisseau.png").convert_alpha()
 vaissea = pygame.transform.scale(vaissea, (100,100))
 monster_img = pygame.transform.scale(monster_img, (130,100))
 monster2_img = pygame.transform.scale(monster2_img, (130,100))
-
+expl = pygame.image.load("Assets/expl.png").convert_alpha()
+expl = pygame.transform.scale(expl, (130,100))
 def color(x, y, color):
    screen.fill(color, (x*16, y*16, 16, 16))
 def draw_vaisseau():
@@ -70,8 +74,38 @@ def draw_vaisseau():
 direction = 1 
 bord_gauche = 1
 bord_droit = 112
+d=0
+def respawn():
+   global d
+   global enemis, enemis_xy 
+   if len(enemis)==0:
+      d+=1
+      if d==20:
 
-     
+         enemis_xy = [ 
+            (1, 10),
+            (1, 20),
+            (1, 30),
+            (11, 10),
+            (11, 20),
+            (11, 30),
+            (21, 10),
+            (21, 20),
+            (21, 30),
+            (31, 10),
+            (31, 20),
+            (31, 30),
+            (41, 10),
+            (41, 20),
+            (41, 30),
+         ]
+
+         enemis=[]
+         for x,y in enemis_xy:
+            rect = pygame.Rect(x * TAILLE, (y * TAILLE)-100, 115, 90)
+            enemis.append(rect)
+         d=0
+
 def debug():
    global tick
    for ennemi in enemis:
@@ -81,35 +115,36 @@ def debug():
    
 def deplacement_enemis():
     global direction
-
+  
+    x,y=display
     if len(enemis) == 0:
         return
-    if tick % 1 == 0:  
-        for ennemi in enemis: 
-            ex, y = ennemi.x, ennemi.y
-            ennemi.x = ex + direction
-            
+
+
+    for ennemi in enemis:
+        ennemi.x += direction
+
+
     toucher_bord = False
-    for x, y in enemis_xy:
-        if x >= bord_droit and direction == 1:
-          
-            
+    for ennemi in enemis:
+        if ennemi.right >= x and direction == 1:  
             toucher_bord = True
             break
-        elif x <= bord_gauche and direction == -1:
+        elif ennemi.left <= 0 and direction == -1:  
             toucher_bord = True
             break
 
 
     if toucher_bord:
-        direction *= -1  
-        for i in range(len(enemis)):
-            x, y = enemis[i]
-
-
+        direction *= -1
+        for ennemi in enemis:
+            ennemi.y += 55
    
 
 def deplacement():
+   global score
+
+  
    keys = pygame.key.get_pressed()
    x, y = vaisseau[0]
    if keys[pygame.K_q]:
@@ -129,13 +164,15 @@ def deplacement():
       vaisseau[0]= (x,46)
    if y>=66:
       vaisseau[0]= (x,65)
-      
-      
+   x,y=display
+   texte1=font.render(f"score:{score}", True, (0, 0, 255))
+   screen.blit(texte1, (x-230,10))
 
 def shoot():
+   global score
    global k
 
-   if k>30:
+   if k>1:
     keys = pygame.key.get_pressed()
     x, y = vaisseau[0]
     if keys[pygame.K_SPACE]:
@@ -144,11 +181,16 @@ def shoot():
         projectiles.append((x, y+1))
         tire.play()
    
-for x, y in projectiles:
-    for ennemi in enemis:
-        if ennemi.collidepoint(x, y):
-            enemis.remove(ennemi)
-            
+   for x, y in projectiles:
+      for ennemi in enemis:
+         if ennemi.collidepoint(x * 16, y * 16):
+               projectiles.remove((x, y))
+          
+               enemis.remove(ennemi)
+               screen.blit(expl, (ennemi.x-4, ennemi.y - 6))
+               boom.play()
+               score+=10
+               
 
   
   
@@ -175,6 +217,7 @@ def main():
        debug()
       deplacement()
       shoot()
+      
       draw_vaisseau()
       move_projectiles()
       deplacement_enemis()
@@ -182,6 +225,7 @@ def main():
       pygame.display.flip()
       k+=1
       tick += 1
+      respawn()
       
   pygame.quit()
 main()
